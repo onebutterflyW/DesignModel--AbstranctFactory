0e0823a0705d80204d5da4f585291dc631aad995
415 <a415@415demacbook-pro.local>
2017-01-10 09:05:17 +0800

    Initial Commit

---
+++ AbstractFactory/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  AbstractFactory
+//
+//  Created by 415 on 17/1/10.
+//  Copyright © 2017年 415. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ AbstractFactory/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  AbstractFactory
+//
+//  Created by 415 on 17/1/10.
+//  Copyright © 2017年 415. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
